01298a81688808d791b46d5886733b6cacde12a7
yerin Jo <76177687+zunjune@users.noreply.github.com>
2022-11-23 22:18:07 +0900

    Update card.js

--- src/main/card.js
+++ src/main/card.js
@@ -6,7 +6,7 @@ function BasicExample() {
   return (
     <Card className='Card'>
       <Card.Img variant="top" className='CardImg'
-        src="/cupcakes-gb1928ac49_640.jpg" />
+        src="/main_food.jpg" />
       <Card.Body style={{textAlign: 'left'}}>
         <Card.Title className='CardTitle'>인하대 베이커리 맛집</Card.Title>
         <Card.Text className='CardText'>디저트</Card.Text>
